7fde440cadee161062565844c728e14610fc99d3
yatinraina <81813515+yatinraina@users.noreply.github.com>
2021-08-14 18:11:57 +0530

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -1,7 +1,7 @@
 import cv2
 
-img = cv2.imread('images.jpg')
-
+cap = cv2.VideoCapture('video1.mp4')
+Threshold = 0.65
 classNames= []
 classFile = 'coco.names'
 with open(classFile,'rt') as f:
@@ -16,17 +16,19 @@ net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
+while True:
+    sucess,img = cap.read()
+    classIds, confs, bbox = net.detect(img,Threshold)
+    print(classIds,bbox)
 
-classIds, confs, bbox = net.detect(img,confThreshold=0.5)
-print(classIds,bbox)
+    if len(classIds) != 0 :
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+            cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.putText(img,str(round(confidence*100,2)),(box[0]+250,box[1]+30),
+                        cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
 
 
-for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
-    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
-    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
-                cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
-
-
-    cv2.imshow("Output",img)
-    cv2.waitKey(0)
-
+        cv2.imshow("Output",img)
+        cv2.waitKey(1)
